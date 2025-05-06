3564da47a61e61ede94f3703e140013e2d32a7a8
Lokesh-141 <166227979+Lokesh-141@users.noreply.github.com>
2025-05-06 21:02:40 +0530

    Create proc_load_silver.sql

---
+++ scripts/silver/proc_load_silver.sql
@@ -0,0 +1,30 @@
+EXEC silver.load_silver
+
+CREATE OR ALTER PROCEDURE silver.load_silver AS
+BEGIN
+    DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME;
+    BEGIN TRY
+        SET @batch_start_time = GETDATE();
+        PRINT '================================================';
+        PRINT 'Loading Silver Layer';
+        PRINT '================================================';
+
+        PRINT '------------------------------------------------';
+        PRINT 'Loading CRM Tables';
+        PRINT '------------------------------------------------';
+
+        -- Loading silver.crm_cust_info
+        SET @start_time = GETDATE();
+        PRINT '>> Truncating Table: silver.crm_cust_info';
+        TRUNCATE TABLE silver.crm_cust_info;
+
+        PRINT '>> Inserting Data Into: silver.crm_cust_info';
+        INSERT INTO silver.crm_cust_info (
+            cst_id,
+            cst_key,
+            cst_firstname,
+            cst_lastname,
+            cst_marital_status,
+            cst_gndr,
+            cst_create_date
+        )
